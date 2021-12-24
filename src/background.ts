c347a883ee704a40c39e7a1f636157170f846980
Reed Krawiec <reedkrawiec@gmail.com>
2021-12-24 02:49:37 -0500

    Basic transfer of frames from content-script to background thread

--- src/background.ts
+++ src/background.ts
@@ -1,23 +1,22 @@
 import * as tf from '@tensorflow/tfjs';
 async function main() {
+    //const response = fetch("data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==");
     const url = chrome.runtime.getURL("my-model.json");
-    console.log(chrome.runtime.getURL("my-model.weights.bin"));
     let model = await tf.loadGraphModel(url);
-    const test_image_url = chrome.runtime.getURL("thumb0002.jpg");
-    console.log(test_image_url);
-    const response = await fetch(test_image_url);
-    console.log(test_image_url);
-    const blob = await response.blob();
-    console.log(blob);
-    const image = await createImageBitmap(blob);
-    const input = tf.image.resizeBilinear(tf.browser.fromPixels(image), [512, 512])
-    .div(255.0).expandDims(0);
-    console.log(input);
-    console.log("test2");
-    model.executeAsync(input).then(res =>{
-        console.log("DONE");
-        console.log(res);
-    });
+    chrome.runtime.onMessage.addListener(async (data, sender)=>{
+        const typed = new Uint8Array(data.frame);
+        const blob = new Blob([typed],{
+            type: "image/jpeg"
+        });
+        const bitmap = await createImageBitmap(blob);
+
+        const input = tf.image.resizeBilinear(tf.browser.fromPixels(bitmap), [512, 512])
+        .div(255.0).expandDims(0);
+        model.executeAsync(input).then(res =>{
+            console.log("DONE " + data.counter);
+            console.log(res);
+        });
+    })
 }
 
 main();
--- src/content-script.ts
+++ src/content-script.ts
@@ -1,9 +1,38 @@
 import * as tf from '@tensorflow/tfjs';
+
 async function main() {
-  
+  let counter = 0;
+
   // @ts-ignore
-  let stream = await navigator.mediaDevices.getDisplayMedia();
+  const stream = await navigator.mediaDevices.getDisplayMedia({
+    audio:false
+  });
+
+  const canvas = <HTMLCanvasElement>document.createElement("canvas");
+  canvas.height = 1200;
+  canvas.width = 1920;
+  const ctx = canvas.getContext("2d");
+
+  const video = <HTMLVideoElement>document.createElement("video");
+  video.src = stream;
+
   const url = chrome.runtime.getURL("my-model.json");
-  console.log(stream);
+  let model = await tf.loadGraphModel(url);
+
+  setInterval(async ()=>{
+  ctx.drawImage(video, 0, 0);
+    const blob:Blob = await new Promise(resolve => canvas.toBlob(resolve,"image/jpeg"));
+    //const bitmap = await createImageBitmap(blob);
+    
+    //const input = tf.image.resizeBilinear(tf.browser.fromPixels(bitmap), [512, 512])
+    //.div(255.0).expandDims(0);
+    const buffer = await blob.arrayBuffer();
+    const typed = new Uint8Array(buffer);
+    chrome.runtime.sendMessage({
+        frame:Array.from(typed),
+        counter
+    });
+    counter++;
+  },2000);
 }
 main();
