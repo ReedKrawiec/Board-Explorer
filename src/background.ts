8dca78ee94f9d52a2306e30622fe23cd6654e2fa
Reed Krawiec <reedkrawiec@gmail.com>
2022-01-04 00:54:39 -0500

    Rendering of board location in content script

--- src/background.ts
+++ src/background.ts
@@ -1,131 +1,134 @@
 import * as tf from '@tensorflow/tfjs';
 
-const names = ["LMS","BOARD","P","R","N","B","Q","K","p","r","n","b","q","k"]
+const names = ["LMS", "BOARD", "P", "R", "N", "B", "Q", "K", "p", "r", "n", "b", "q", "k"]
 const [modelWidth, modelHeight] = [512, 512];
 
+async function parseBoardImage(model: tf.GraphModel, image: ImageBitmap) {
+
+    const input = tf.image.resizeBilinear(tf.browser.fromPixels(image), [512, 512])
+        .div(255.0).expandDims(0);
+    const res: Array<any> = <Array<any>>await model.executeAsync(input);
+    let pieces = [];
+    /*
+    let final:{
+        [index:string]:Array<Array<unknown>>
+    } = {};
+    */
+    let final: any = {};
+    const boxes = res[0].arraySync()[0];
+    const probabilitys = res[1].arraySync()[0];
+    const classes = res[2].arraySync()[0];
+    const entries = res[3].arraySync()[0];
+    for (let a = 0; a < entries; a++) {
+        const class_name = names[classes[a]];
+        if (!final[class_name]) {
+            final[class_name] = [];
+        }
+        final[class_name].push(boxes[a]);
+    }
+    let board_info: {
+        width: number,
+        height: number,
+        x: number,
+        y: number
+    };
+    //let boxes = data.map((d)=>[])
+    let by_class = {};
+    //["LAST_MOVE_SQUARE","BOARD","P","R","N","B","Q","K","p","r","n","b","q","k"]
+    let max_per_class = [2, 1, 8, 8, 8, 8, 8, 1, 8, 8, 8, 8, 8, 1]
+    for (let key of names) {
+        if (final[key] && (true || !["LMS"].includes(key))) {
+            for (let index = 0; index < final[key].length; index++) {
+                let box = final[key][index];
+                let [x1, y1, x2, y2] = box;
+                const width = x2 - x1;
+                const height = y2 - y1;
+                const klass = key;
+                const score = 1;
+                if (key === "BOARD") {
+                    board_info = {
+                        x: x1 + width / 2,
+                        y: y1 + height / 2,
+                        width,
+                        height
+                    }
+                }
+                else if (!["BOARD", "LMS"].includes(key))
+                    pieces.push({
+                        type: key,
+                        width,
+                        height,
+                        x: x1 + width / 2,
+                        y: y1 + height / 2
+                    })
+            }
+        }
+    }
+    let board = [
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""]
+    ];
+    if (board_info && true) {
+        let board_top_left = {
+            x: board_info.x - board_info.width / 2,
+            y: board_info.y - board_info.height / 2
+        }
+        for (let piece of pieces) {
+            let piece_cord = {
+                x: ((piece.x - board_top_left.x)),
+                y: ((piece.y - board_top_left.y))
+            }
+            let cords = {
+                x: Math.floor(piece_cord.x / (board_info.width / 8)),
+                y: Math.floor(piece_cord.y / (board_info.height / 8))
+            }
+            board[cords.y][cords.x] = piece.type;
+        }
+    }
+    let fen = ""
+    for (let row of board) {
+        let tracker = 0;
+        if (fen !== "")
+            fen = `${fen}/`;
+        for (let entry of row) {
+            if (entry === "") {
+                tracker++;
+            }
+            else {
+                if (tracker === 0) {
+                    fen = `${fen}${entry}`;
+                }
+                else {
+                    fen = `${fen}${tracker}${entry}`;
+                }
+                tracker = 0;
+            }
+        }
+        if (tracker !== 0) {
+            fen = `${fen}${tracker}`
+        }
+    }
+    return { fen, board_info }
+}
 async function main() {
     //const response = fetch("data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==");
     const url = chrome.runtime.getURL("my-model.json");
     let model = await tf.loadGraphModel(url);
-    chrome.runtime.onMessage.addListener(async (data, sender)=>{
+    chrome.runtime.onMessage.addListener(async (data, sender) => {
         const typed = new Uint8Array(data.frame);
-        const blob = new Blob([typed],{
+        const blob = new Blob([typed], {
             type: "image/jpeg"
         });
         const bitmap = await createImageBitmap(blob);
-
-        const input = tf.image.resizeBilinear(tf.browser.fromPixels(bitmap), [512, 512])
-        .div(255.0).expandDims(0);
-        model.executeAsync(input).then((res:Array<any>)=>{
-            let pieces = [];
-            /*
-            let final:{
-                [index:string]:Array<Array<unknown>>
-            } = {};
-            */
-            let final:any = {};
-            const boxes = res[0].arraySync()[0];
-            const probabilitys = res[1].arraySync()[0];
-            const classes = res[2].arraySync()[0];
-            const entries = res[3].arraySync()[0];
-            for(let a = 0; a < entries; a++){
-                const class_name = names[classes[a]];
-                if(!final[class_name]){
-                    final[class_name] = [];
-                }
-                final[class_name].push(boxes[a]);
-            }
-            let board_info;
-            //let boxes = data.map((d)=>[])
-            let by_class = {};
-            //["LAST_MOVE_SQUARE","BOARD","P","R","N","B","Q","K","p","r","n","b","q","k"]
-            let max_per_class = [2,1,8,8,8,8,8,1,8,8,8,8,8,1]
-            console.log(final);
-            for(let key of names){
-                if(final[key] && (true || !["LMS"].includes(key))){
-                    console.log(final[key]);
-                    console.log(final[key].entries())
-                    for(let index = 0; index < final[key].length;index++){
-                        let box = final[key][index];
-                        console.log("ya");
-                        console.log(box);
-                        let [x1, y1, x2, y2] = box;
-                        const width = x2 - x1;
-                        const height = y2 - y1;
-                        const klass = key;
-                        const score = 1;
-                        console.log(key);
-                        if(key === "BOARD"){
-                            board_info = {
-                                x: x1 + width/2,
-                                y: y1 + height/2,
-                                width,
-                                height
-                            }
-                        }
-                        else if(!["BOARD","LMS"].includes(key))
-                            pieces.push({
-                                type:key,
-                                width,
-                                height,
-                                x: x1 + width/2,
-                                y: y1 + height/2
-                            })
-                    }
-                }        
-            }
-            let board = [
-                ["","","","","","","",""],
-                ["","","","","","","",""],
-                ["","","","","","","",""],
-                ["","","","","","","",""],
-                ["","","","","","","",""],
-                ["","","","","","","",""],
-                ["","","","","","","",""],
-                ["","","","","","","",""]
-            ];
-            console.log(pieces);
-            if(board_info && true){
-                let board_top_left = {
-                    x:board_info.x - board_info.width/2,
-                    y:board_info.y - board_info.height/2
-                }
-                for(let piece of pieces){  
-                    let piece_cord = {
-                        x:((piece.x - board_top_left.x)),
-                        y:((piece.y - board_top_left.y))
-                    }
-                    let cords = {
-                        x:Math.floor(piece_cord.x/(board_info.width/8)),
-                        y:Math.floor(piece_cord.y/(board_info.height/8))
-                    }
-                    board[cords.y][cords.x] = piece.type;
-                }
-            }
-            let fen = ""
-            for(let row of board){
-                let tracker = 0;
-                if(fen !== "")
-                    fen = `${fen}/`;
-                for(let entry of row){
-                    if(entry === ""){
-                        tracker++;
-                    }
-                    else{
-                        if(tracker === 0){
-                            fen = `${fen}${entry}`;
-                        }
-                        else{
-                            fen = `${fen}${tracker}${entry}`;
-                        }
-                        tracker = 0;
-                    }
-                }
-                if(tracker !== 0){
-                    fen = `${fen}${tracker}`
-                }
-            }
-            console.log(fen);
+        const { fen, board_info } = await parseBoardImage(model, bitmap)
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { fen, board_info }, function (response) { });
         });
     })
 }
--- src/content-script.ts
+++ src/content-script.ts
@@ -1,51 +1,71 @@
 import * as tf from '@tensorflow/tfjs';
+const [imagewidth, imageheight] = [1920, 1200]
+const [screenWidth,screenHeight] = [1920,1200];
+const gutterHeight = (screenWidth - document.body.clientHeight) / 2;
 
-const [imagewidth, imageheight] = [1920,1200]
+const cover = document.createElement("div");
+cover.id = "board";
+cover.style.border = "solid 10px black";
+cover.style.position = "absolute";
 
+document.body.appendChild(cover);
+
+
+
+chrome.runtime.onMessage.addListener(async (data, sender) => {
+    const {fen,board_info} = data;
+    console.log(board_info)
+    const boardSize = board_info.width * screenWidth;
+    cover.style.width = boardSize + "px";
+    cover.style.height = boardSize + "px";
+    cover.style.left = board_info.x * screenWidth - boardSize/2 + "px";
+    cover.style.top = board_info.y * screenWidth - boardSize/2 - gutterHeight + "px";
+    return true;
+})
 async function main() {
-  let counter = 0;
+    let counter = 0;
 
-  // @ts-ignore
-  const stream = await navigator.mediaDevices.getDisplayMedia({
-    audio:false
-  });
+    // @ts-ignore
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+        audio: false
+    });
 
-  const canvas = <HTMLCanvasElement>document.createElement("canvas");
-  canvas.height = 512;
-  canvas.width = 512;
-  const ctx = canvas.getContext("2d");
+    const canvas = <HTMLCanvasElement>document.createElement("canvas");
+    canvas.height = 512;
+    canvas.width = 512;
+    const ctx = canvas.getContext("2d");
 
-  const video = <HTMLVideoElement>document.createElement("video");
-  video.srcObject = stream;
-  await video.play();
-  const url = chrome.runtime.getURL("my-model.json");
-  let model = await tf.loadGraphModel(url);
+    const video = <HTMLVideoElement>document.createElement("video");
+    video.srcObject = stream;
+    await video.play();
+    const url = chrome.runtime.getURL("my-model.json");
+    let model = await tf.loadGraphModel(url);
 
-  setInterval(async ()=>{
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      ctx.fillStyle = "#000000";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      const new_width = 512;
-      const new_height = 512/imagewidth * imageheight;
-      ctx.drawImage(
-          video,
-          512/2 - new_width/2,
-          512/2 - new_height/2,
-          new_width,
-          new_height
-      );
-      const blob:Blob = await new Promise(resolve => canvas.toBlob(resolve,"image/jpeg"));
-      //const bitmap = await createImageBitmap(blob);
+    setInterval(async () => {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        const new_width = 512;
+        const new_height = 512 / imagewidth * imageheight;
+        ctx.drawImage(
+            video,
+            512 / 2 - new_width / 2,
+            512 / 2 - new_height / 2,
+            new_width,
+            new_height
+        );
+        const blob: Blob = await new Promise(resolve => canvas.toBlob(resolve, "image/jpeg"));
+        //const bitmap = await createImageBitmap(blob);
 
-      //const input = tf.image.resizeBilinear(tf.browser.fromPixels(bitmap), [512, 512])
-      //.div(255.0).expandDims(0);
-      const buffer = await blob.arrayBuffer();
-      const typed = new Uint8Array(buffer);
-      chrome.runtime.sendMessage({
-          frame:Array.from(typed),
-          counter
-      });
-      counter++;
-  },2000);
+        //const input = tf.image.resizeBilinear(tf.browser.fromPixels(bitmap), [512, 512])
+        //.div(255.0).expandDims(0);
+        const buffer = await blob.arrayBuffer();
+        const typed = new Uint8Array(buffer);
+        chrome.runtime.sendMessage({
+            frame: Array.from(typed),
+            counter
+        });
+        counter++;
+    }, 2000);
 }
 main();
