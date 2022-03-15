d280bb065c3debfb6cf8b5f9821564fe6104eff6
Reed Krawiec <reedkrawiec@gmail.com>
2022-03-15 00:21:52 -0400

    Consistent UI

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -2,6 +2,23 @@ let isToggled = false;
 let isEvaluating = false;
 let isPlayable = false;
 
+chrome.storage.local.get(["enabled","evaluating","playable"]).then(function(result){
+  console.log(result);
+  isEvaluating = result.evaluating;
+  if(isEvaluating){
+    toggleView(document.querySelector("#eval").classList,true);
+  }
+  isPlayable = result.playable;
+  if(isPlayable){
+    toggleView(document.querySelector("#playable").classList,true);
+  }
+  isToggled = result.enabled;
+  if(isToggled){
+    toggleView(document.querySelector("#toggle").classList,true);
+  }
+  console.log(`ADHIUAWHIUDA ${isToggled} ${isEvaluating} ${isPlayable}`);
+});
+
 function toggleView(classlist,state){
   if(state){
     classlist.add("on");
@@ -13,19 +30,20 @@ function toggleView(classlist,state){
   }
 }
 
-document.querySelector("#toggle").addEventListener("click", function() {
+document.querySelector("#toggle").addEventListener("click",async function() {
   isToggled = !isToggled;
   let classlist = document.querySelector("#toggle").classList
   toggleView(classlist,isToggled);
   chrome.runtime.sendMessage("toggle");
+  await chrome.storage.local.set({"enabled":isToggled});
 });
-document.querySelector("#eval").addEventListener("click", function() {
+document.querySelector("#eval").addEventListener("click",async function() {
   isEvaluating = !isEvaluating;
   let classlist = document.querySelector("#eval").classList
   toggleView(classlist,isEvaluating);
   chrome.runtime.sendMessage("eval");
 });
-document.querySelector("#playable").addEventListener("click", function() {
+document.querySelector("#playable").addEventListener("click",async function() {
   isPlayable = !isPlayable;
   let classlist = document.querySelector("#playable").classList
   toggleView(classlist,isPlayable);
--- src/background.ts
+++ src/background.ts
@@ -18,6 +18,12 @@ enum Perspective {
     black,
 }
 
+console.log("INIT");
+chrome.storage.local.set({
+    evaluating: false,
+    playable: false,
+    enabled:false
+})
 function getTurn(board:string[][]):Perspective {
     let whiteKingSide = "top";
     let blackKingSide = "top"
@@ -136,59 +142,6 @@ async function parseBoardImage(model: tf.GraphModel, image: ImageBitmap) {
                     }
                 }
                 else if (!["BOARD", "LMS"].includes(key)){
-                    /*
-                    const color_samples = [
-                        {
-                            x: x1 * 512,
-                            y: y1 * 512,
-                        },
-                        {
-                            x: x2 * 512,
-                            y: y1 * 512,
-                        },
-                        {
-                            x: x1 * 512,
-                            y: y2 * 512,
-                        },
-                        {
-                            x: x2 * 512,
-                            y: y2 * 512,
-                        }    
-                    ]
-                    const sample_list:{
-                        [key:string]:{
-                            count:number,
-                            color:string
-                        }
-                    } = {};
-                    for(let sample of color_samples) {
-                        let p = ctx.getImageData(sample.x, sample.y, 1, 1).data;
-                        let q = rgbRound(p[0], p[1], p[2]);
-                        let hex = "#" + rgbToHex(q[0], q[1], q[2]);
-                        let pre = hex.slice(0,2);
-                        if(!sample_list[hex]) {
-                            sample_list[hex] = {
-                                count: 1,
-                                color: hex
-                            }
-                        }
-                        else {
-                            sample_list[hex].count++;
-          document.querySelector("#eval").addEventListener("click", function() {
-  chrome.runtime.sendMessage("eval");
-  console.log("eval");
-});              }
-                        if(!global_sample_result_map[hex]){
-                            global_sample_result_map[hex] = {
-                                count: 1,
-                                cords: [[x1,y1,x2,y2]]
-                            };
-                        } else {
-                            global_sample_result_map[hex].cords.push([x1,y1,x2,y2]);
-                        }
-                    }
-                    console.log(sample_list);
-                    */
                     pieces.push({
                         type: key,
                         width,
@@ -300,6 +253,12 @@ async function parseBoardImage(model: tf.GraphModel, image: ImageBitmap) {
     return { fen, board_info, perspective, last_moved, numDiffs: diffs.length };
 }
 
+chrome.storage.local.set({
+    "enabled": false,
+    "evaluating": false,
+    "playable": false
+})
+
 async function main() {
     //const response = fetch("data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==");
     const url = chrome.runtime.getURL("model/my-model.json");
@@ -307,11 +266,19 @@ async function main() {
     const canvas = new OffscreenCanvas(512,512);
     chrome.runtime.onMessage.addListener(async (data, sender) => {
         if(data == "eval"){
+            const curr = await chrome.storage.local.get("evaluating")
+            chrome.storage.local.set({
+                evaluating: !curr.evaluating,
+            });
             chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, "eval", function (response) { });
             });
         }
         else if (data == "playable"){
+            const curr = await chrome.storage.local.get("playable")
+            chrome.storage.local.set({
+                playable: !curr.playable,
+            });
             chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, "playable", function (response) { });
             });
