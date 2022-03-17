d0ffbc6b33788d9aa685d7b4c538026378bd5d85
Reed Krawiec <reedkrawiec@gmail.com>
2022-03-17 00:25:23 -0400

    Restructured background.ts and enable on all sites

--- src/background.ts
+++ src/background.ts
@@ -3,27 +3,26 @@ import * as tf from '@tensorflow/tfjs';
 const names = ["BOARD", "p", "r", "n", "b", "q", "k", "P", "R", "N", "B", "Q", "K"]
 const [modelWidth, modelHeight] = [512, 512];
 
-function rgbToHex(r:number, g:number, b:number) {
-    if (r > 255 || g > 255 || b > 255)
-        throw "Invalid color component";
-    return ((r << 16) | (g << 8) | b).toString(16);
-}
-
-function rgbRound(r:number, g:number, b:number) {
-    return [r, g, b].map(x => Math.min(Math.round(x/36) * 36, 255))
-}
-
 enum Perspective {
     white,
     black,
 }
 
-console.log("INIT");
-chrome.storage.local.set({
-    evaluating: false,
-    playable: false,
-    enabled:false
-})
+interface piece {
+    type: string,
+    width: number,
+    height: number,
+    x: number,
+    y: number
+}
+
+interface boardLocation {
+    width: number,
+    height: number,
+    x: number,
+    y: number
+}
+
 function getTurn(board:string[][]):Perspective {
     let whiteKingSide = "top";
     let blackKingSide = "top"
@@ -81,152 +80,7 @@ function getTurn(board:string[][]):Perspective {
     return Perspective.white;
 }
 
-let last_board:string[][];
-let last_moved_cache:string;
-async function parseBoardImage(model: tf.GraphModel, image: ImageBitmap) {
-    const canvas = new OffscreenCanvas(512, 512);
-    const ctx = canvas.getContext("2d");
-    ctx.drawImage(image, 0, 0);
-    const input = tf.image.resizeBilinear(tf.browser.fromPixels(image), [512, 512])
-        .div(255.0).expandDims(0);
-    const res: any = <any>await model.executeAsync(input);
-    let pieces = [];
-    let final: any = {};
-    input.dispose();
-    const boxes = res[0].arraySync()[0];
-    const probabilitys = res[1].arraySync()[0];
-    const classes = res[2].arraySync()[0];
-    const entries = res[3].arraySync()[0];
-    for(let tensor of res){
-        tensor.dispose();
-    }
-    const global_sample_result_map:{
-        [key:string]:{
-            count:number,
-            cords:number[][]
-        }
-    } = {};
-    for (let a = 0; a < entries; a++) {
-        const class_name = names[classes[a]];
-        if (!final[class_name]) {
-            final[class_name] = [];
-        }
-        final[class_name].push(boxes[a]);
-    }
-    let board_info: {
-        width: number,
-        height: number,
-        x: number,
-        y: number
-    };
-    //let boxes = data.map((d)=>[])
-    let by_class = {};
-    //["LAST_MOVE_SQUARE","BOARD","P","R","N","B","Q","K","p","r","n","b","q","k"]
-    let max_per_class = [2, 1, 8, 8, 8, 8, 8, 1, 8, 8, 8, 8, 8, 1]
-    for (let key of names) {
-        if (final[key] && (true || !["LMS"].includes(key))) {
-            for (let index = 0; index < final[key].length; index++) {
-                let box = final[key][index];
-                let [x1, y1, x2, y2] = box;
-                const width = x2 - x1;
-                const height = y2 - y1;
-                const klass = key;
-                const score = 1;
-                
-                if (key === "BOARD") {
-                    board_info = {
-                        x: x1 + width / 2,
-                        y: y1 + height / 2,
-                        width,
-                        height
-                    }
-                }
-                else if (!["BOARD", "LMS"].includes(key)){
-                    pieces.push({
-                        type: key,
-                        width,
-                        height,
-                        x: x1 + width / 2,
-                        y: y1 + height / 2
-                    })   
-                }
-            }
-        }
-    }
-    let board = [
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""]
-    ];
-    if (board_info && true) {
-        let board_top_left = {
-            x: board_info.x - board_info.width / 2,
-            y: board_info.y - board_info.height / 2
-        }
-        for (let piece of pieces) {
-            let piece_cord = {
-                x: ((piece.x - board_top_left.x)),
-                y: ((piece.y - board_top_left.y))
-            }
-            let cords = {
-                x: Math.floor(piece_cord.x / (board_info.width / 8)),
-                y: Math.floor(piece_cord.y / (board_info.height / 8))
-            }
-            board[cords.y][cords.x] = piece.type;
-        }
-    }
-    let last_moved = "";
-    
-    let perspective = getTurn(board);
-    if(perspective === Perspective.black) {
-        board = board.map((row)=>row.reverse()).reverse();
-    }
-    let diffs:number[][] = [];
-    if(last_board){
-        for(let [y,row] of board.entries()){
-            for(let [x, val] of row.entries()){
-                if(val != last_board[y][x]){
-                    diffs.push([y,x]);
-                }
-            }
-        }
-    }
-    const lowerCaseRegex = /[a-z]/
-    let black_count = 0;
-    let white_count = 0;
-    for (let diff of diffs) {
-        let [y, x] = diff;
-        if (board[y][x] != "") {
-            if (board[y][x].match(lowerCaseRegex)) {
-                black_count++;
-            } else {
-                white_count++;
-            }
-        }
-    }
-    if (diffs.length > 0) {
-        console.log("///////////////")
-        console.log(black_count);
-        console.log(white_count);
-        console.log(last_moved_cache);
-        if (black_count > white_count) {
-            last_moved = "b";
-            last_moved_cache = "b";
-        } else if (white_count > black_count) {
-            last_moved = "w";
-            last_moved_cache = "w";
-        } else if(last_moved_cache === "w" || last_moved_cache === "b"){
-            last_moved = last_moved_cache;
-        } else {
-            last_moved = "w";
-        }
-    }
-    last_board = board;
+const createFen = (board:string[][]) => {
     let fen = ""
     for (let row of board) {
         let tracker = 0;
@@ -250,22 +104,168 @@ async function parseBoardImage(model: tf.GraphModel, image: ImageBitmap) {
             fen = `${fen}${tracker}`
         }
     }
+    return fen;
+}
+
+const boardDiff = (board1: string[][], board2: string[][]) => {
+    if(board2 == null) {
+        return {diffs:[], black_count:0, white_count:0}
+    }
+    const lowerCaseRegex = /[a-z]/
+    const diffs = [];
+    let black_count = 0;
+    let white_count = 0;
+    for (let [y, row] of board1.entries()) {
+        for (let [x, val] of row.entries()) {
+            if (val != board2[y][x]) {
+                diffs.push([y, x]);
+                if (val != "") {
+                    if (val.match(lowerCaseRegex)) {
+                        black_count++;
+                    } else {
+                        white_count++;
+                    }
+                }
+            }
+        }
+    }
+    return {diffs, black_count, white_count};
+}
+
+const determineLastMoved = (last_moved_cache:string,white_count:number,black_count:number) => {
+    if (black_count > white_count) {
+        return {last_moved: "b", cache: "b"}
+    } else if (white_count > black_count) {
+        return {last_moved: "w", cache: "w"}
+    } else if(last_moved_cache === "w" || last_moved_cache === "b"){
+        return {last_moved: last_moved_cache, cache: last_moved_cache}
+    } else {
+        return {last_moved: "w", cache: null}
+    }
+}
+
+const createBoardArray = (pieces:piece[],board_info:boardLocation) => {
+    let board = [
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""]
+    ];
+    let board_top_left = {
+        x: board_info.x - board_info.width / 2,
+        y: board_info.y - board_info.height / 2
+    }
+    for (let piece of pieces) {
+        let piece_cord = {
+            x: ((piece.x - board_top_left.x)),
+            y: ((piece.y - board_top_left.y))
+        }
+        let cords = {
+            x: Math.floor(piece_cord.x / (board_info.width / 8)),
+            y: Math.floor(piece_cord.y / (board_info.height / 8))
+        }
+        board[cords.y][cords.x] = piece.type;
+    }
+    return board;
+}
+
+const parseTensorResults = (entries:any,boxes:any,classes:any) => {
+    let board_info: boardLocation;
+    let pieces: piece[] = [];
+    for (let a = 0; a < entries; a++) {
+        const class_name = names[classes[a]];
+        let box = boxes[a];
+        let [x1, y1, x2, y2] = box;
+        const width = x2 - x1;
+        const height = y2 - y1;
+        if (class_name === "BOARD") {
+            board_info = {
+                x: x1 + width / 2,
+                y: y1 + height / 2,
+                width,
+                height
+            }
+        }
+        else {
+            pieces.push({
+                type: class_name,
+                width,
+                height,
+                x: x1 + width / 2,
+                y: y1 + height / 2
+            })
+        }
+    }
+    return {board_info, pieces};
+}
+
+const parseRawBoardImage = async (model: tf.GraphModel, image:ImageBitmap) => {
+    tf.engine().startScope()
+    const canvas = new OffscreenCanvas(512, 512);
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(image, 0, 0);
+    const input = tf.image.resizeBilinear(tf.browser.fromPixels(image), [512, 512])
+        .div(255.0).expandDims(0);
+    const res: any = <any>await model.executeAsync(input);
+    input.dispose();
+    const boxes = res[0].arraySync()[0];
+    const classes = res[2].arraySync()[0];
+    const entries = res[3].arraySync()[0];
+    for(let tensor of res){
+        tensor.dispose();
+    }
+    tf.engine().endScope();
+    return {boxes,classes,entries}
+}
+
+let last_board:string[][];
+let last_moved_cache:string;
+async function parseBoardImage(model: tf.GraphModel, image: ImageBitmap) {
+    const {boxes,classes,entries} = await parseRawBoardImage(model,image);
+    let {board_info, pieces} = await parseTensorResults(entries, boxes, classes);
+    console.log(board_info);
+    console.log(pieces);
+    if(!board_info){
+        return null;
+    }
+    let board = createBoardArray(pieces,board_info);
+    let perspective = getTurn(board);
+    if(perspective === Perspective.black) {
+        board = board.map((row)=>row.reverse()).reverse();
+    }
+    let last_moved;
+    const {diffs, black_count, white_count} = boardDiff(board, last_board);
+    if (diffs.length > 0) {
+        console.log("///////////////")
+        console.log(black_count);
+        console.log(white_count);
+        console.log(last_moved_cache);    
+        let {last_moved:move, cache} = determineLastMoved(last_moved_cache,board.reduce((acc,row)=>acc+row.filter((val)=>val!=="").length,0),board.reduce((acc,row)=>acc+row.filter((val)=>val!=="").length,0));
+        last_moved = move;
+        last_moved_cache = cache;
+    }
+    last_board = board;
+    const fen = createFen(board);
     return { fen, board_info, perspective, last_moved, numDiffs: diffs.length };
 }
 
-chrome.storage.local.set({
-    "enabled": false,
-    "evaluating": false,
-    "playable": false
-})
+const frameToBitmap = async (frame: any): Promise<ImageBitmap> => {
+    const typed = new Uint8Array(frame);
+    const blob = new Blob([typed], {
+        type: "image/jpeg"
+    });
+    const bitmap = await createImageBitmap(blob);
+    return bitmap;
+}
 
 async function main() {
-    //const response = fetch("data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==");
     const url = chrome.runtime.getURL("model/my-model.json");
     let model:tf.GraphModel = await tf.loadGraphModel(url);
-    const canvas = new OffscreenCanvas(512,512);
     chrome.runtime.onMessage.addListener(async (data, sender) => {
-        console.log(data);
         if (data == "toggle") {
             const curr = await chrome.storage.local.get("enabled")
             chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -300,16 +300,12 @@ async function main() {
             });
         }
         else{
-            const typed = new Uint8Array(data.frame);
-            const blob = new Blob([typed], {
-                type: "image/jpeg"
-            });
-            const bitmap = await createImageBitmap(blob);
-            tf.engine().startScope()
-            const { fen, board_info, perspective, last_moved, numDiffs } = await parseBoardImage(model, bitmap);
-            tf.engine().endScope()
-            console.log(tf.memory().numTensors);
-            console.log(last_moved +" LAST MOVE");
+            const bitmap = await frameToBitmap(data.frame);
+            const result = await parseBoardImage(model, bitmap);
+            if(!result){
+                return;
+            }
+            const {fen,board_info,perspective,last_moved,numDiffs} = result;
             if(numDiffs > 0) {
                 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
                     chrome.tabs.sendMessage(tabs[0].id, { fen, board_info, perspective, last_moved }, function (response) { });
@@ -319,4 +315,11 @@ async function main() {
         return true;
     })
 }
+
+chrome.storage.local.set({
+    "enabled": false,
+    "evaluating": false,
+    "playable": false
+})
+
 main();
